7af090deb97aefad6ca502cd4364146a8135407c
Matheus Afonso <84944531+MatAfonso@users.noreply.github.com>
2025-03-18 19:07:05 +0000

    Implementar 3 destas funcionalidades listadas para este simulador de um ERP Financeiro

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from faker import Faker
 def main():
     st.title("ERP Financeiro com Streamlit")
     
-    menu = ["Clientes", "Contas a Pagar", "Contas a Receber", "Lançamentos", "Relatórios"]
+    menu = ["Clientes", "Contas a Pagar", "Contas a Receber", "Lançamentos", "Relatórios", "Contas a Pagar por Fornecedor", "Top Clientes", "Receita vs Despesa"]
     choice = st.sidebar.selectbox("Selecione uma opção", menu)
     conn = sqlite3.connect("erp_finance.db", detect_types=sqlite3.PARSE_DECLTYPES)
     cursor = conn.cursor()
@@ -36,6 +36,60 @@ def main():
         st.subheader("Relatório de Fluxo de Caixa")
         df = pd.read_sql_query("SELECT tipo, SUM(valor) as total FROM lancamentos GROUP BY tipo", conn)
         st.dataframe(df)
+
+    elif choice == "Contas a Pagar por Fornecedor":
+        st.subheader("Distribuição das Contas a Pagar por Fornecedor")
+    
+        query = "SELECT fornecedor, SUM(valor) as total FROM contas_pagar GROUP BY fornecedor ORDER BY total DESC"
+        df = pd.read_sql_query(query, conn)
+
+        if df.empty:
+            st.warning("Nenhum dado disponível para exibição.")
+        else:
+            chart_type = st.radio("Escolha o tipo de gráfico:", ("Pizza", "Barras"))
+
+        if chart_type == "Pizza":
+            fig = df.set_index("fornecedor").plot.pie(y="total", autopct='%1.1f%%', startangle=90, figsize=(10, 10), legend = False)
+            st.pyplot(fig.figure)
+        
+        elif chart_type == "Barras":
+            st.bar_chart(df.set_index("fornecedor"))
+
+    elif choice == "Top Clientes":
+        st.subheader("Top 5 Clientes com Maior Receita")
+
+        query = """
+        SELECT c.nome, SUM(cr.valor) as total_receita 
+        FROM contas_receber cr
+        JOIN clientes c ON cr.cliente_id = c.id 
+        GROUP BY c.id, c.nome
+        ORDER BY total_receita DESC 
+        LIMIT 5
+        """
+        df = pd.read_sql_query(query, conn)
+
+       
+        st.write("Tabela:")
+        st.dataframe(df)
+            
+        st.write("Gráfico de Barras:")
+        st.bar_chart(df.set_index("nome"))
+
+    elif choice == "Receita vs Despesa":
+        st.subheader("Mês Atual")
+        
+        query = """
+        SELECT tipo, SUM(valor) as total 
+        FROM lancamentos 
+        WHERE strftime('%Y-%m', data) = strftime('%Y-%m', 'now')
+        GROUP BY tipo
+        """
+        df = pd.read_sql_query(query, conn)
+        
+    
+        st.write("Gráfico de Barras Receita vs Despesa:")
+        st.bar_chart(df.set_index("tipo"))    
+   
     
     conn.close()
     
